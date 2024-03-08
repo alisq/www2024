6b1507d9d2051c8eead9cb3b4d51a3edc7974651
alisq <ali.shamas.qadeer@gmail.com>
2024-03-08 14:15:41 -0500

    added js

--- JS-LESSONS/week-01/js/script.js
+++ JS-LESSONS/week-01/js/script.js
@@ -1,6 +1,36 @@
+
+
 let message = 'hello world';
 // to get to the console, just type in CMD+OPT+J or CTRL+ALT+J
 console.log(message)
+
+
+$(document).scroll(function(){
+    let fromTop = $(document).scrollTop()/10;
+    $(".second-hand").css("transform","rotate("+fromTop+"deg)")
+})
+
+
+
+$(".student").hover(function(){
+    if ( ! $(this).hasClass("hovered")) {
+        let name = $(this).text();
+        let interaction = $(this).attr("data-inter");    
+        let newName = name +" - "+interaction;
+        $(this)
+        .text(newName)
+        .addClass("hovered");
+    }
+    
+
+})
+
+
+
+
+
+
+
 
 
 $(".student").click(function(){
@@ -13,7 +43,11 @@ $(".student").click(function(){
     if (name == "River Carley") {
         $("body").toggleClass("blueBG");
     } else {
-        alert("wait, you're not River. You're "+name)
+        // alert("wait, you're not River. You're "+name)
     }
     
+})
+
+$(".circle").click(function(){
+    $(this).toggleClass('active')
 })
