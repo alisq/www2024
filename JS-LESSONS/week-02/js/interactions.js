7944f34226628fcedb7a4e8ef5e27d3a2ac5d13c
alisq <ali.shamas.qadeer@gmail.com>
2024-03-12 13:57:05 -0400

    added interactions

---
+++ JS-LESSONS/week-02/js/interactions.js
@@ -0,0 +1,55 @@
+//CLICK
+//
+$("#click_circle").click(function(){
+    $("#cursor_circle").toggleClass("clickedOn")
+})
+
+$(".wb").click(function(){
+    $(this).toggleClass("invisible")
+})
+
+$(document).mousemove(function(e){
+    $("#cursor_circle").css({
+        "top":e.clientY,
+        "left":e.clientX
+    })
+})
+
+$("#show_hide").hover(function(){
+    $("#cursor_circle").addClass("clickedOn")
+}, function(){
+    $("#cursor_circle").removeClass("clickedOn")
+})
+
+$("#scroll").scroll(function(){
+    $("#information_circle").text($(this).scrollTop()+"px");
+})
+
+let message="";
+$(".select").click(function(){
+    $(this).toggleClass("selected")
+    message = message+$(this).text()+" ";
+    console.log(message)
+    if (message == "hello world  my name is ali this class is graphic design ") {
+        alert(message);
+        $(".selected").removeClass("selected");
+        message="";
+    }
+})
+
+
+
+let written = "";
+$(document).keyup(function(event){
+
+    written= written+event.key;
+    console.log(written)
+})
+
+$("#drag").draggable();
+$("#drop").droppable({
+    drop:function(){
+        $("#drag").addClass("dropped")
+        $(this).addClass("dropped")
+     }
+})
