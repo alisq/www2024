8e4f49d291055d2594cac7c3314d2b76b1bd3345
alisq <ali.shamas.qadeer@gmail.com>
2024-03-22 12:24:44 -0400

    added empty content

---
+++ MEDIA-QUERIES/js/script.js
@@ -0,0 +1,8 @@
+$(".random-red").each(function(){
+
+    $(this).css({
+        "top":Math.floor(Math.random()*$(window).height()),
+        "left":Math.floor(Math.random()*$(window).width()),
+    })
+
+})
