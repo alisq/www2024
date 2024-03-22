d3fd8d7392ab49dfa9029d4447146adf81cbb3e9
alisq <ali.shamas.qadeer@gmail.com>
2024-03-22 13:03:05 -0400

    added content.

--- MEDIA-QUERIES/js/script.js
+++ MEDIA-QUERIES/js/script.js
@@ -1,8 +1,10 @@
+
+
 $(".random-red").each(function(){
 
     $(this).css({
         "top":Math.floor(Math.random()*$(window).height()),
-        "left":Math.floor(Math.random()*$(window).width()),
+        "left":Math.floor(Math.random()*$(window).width())
     })
 
 })
